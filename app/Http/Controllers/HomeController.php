ab0f750d57aef505c649188989c2c34af60680ae
Yumi Shikata <yumishika17@gmail.com>
2021-05-12 15:35:13 +0200

    Controller e Model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Dress;
+
+class HomeController extends Controller
+{
+    
+        
+    public function products() 
+    {
+        $products = Dress::all();
+        $data = [
+            'products'=> $products
+        ];
+        return view('products', $data);
+    }
+}
